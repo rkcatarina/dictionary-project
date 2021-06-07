2d2002e2e09654fbb4ee582f4f90de83aaa36c54
CatarinaRK <79782399+rkcatarina@users.noreply.github.com>
2021-06-07 16:39:43 +0200

    Linked GitHub and Netlify in footer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    
     <div className="App">
       <div className="container">
         <header className="App-header">
           <strong>What do you want to look up?</strong>
         </header>
         <main>
-          <Dictionary defaultWord={"wine"}/>
+          <Dictionary defaultWord={"wine"} />
         </main>
         <footer className="App-footer">
-          Coded by Catarina Redshaw Kranich
+          Coded by Catarina Redshaw Kranich and{" "}
+          <a href="https://github.com/rkcatarina/dictionary-project">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://zealous-pasteur-410dc8.netlify.app/">
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
